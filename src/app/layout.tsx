6ed1b26486f91638313767c437a325b11822c242
m74jji <pytaek1@gmail.com>
2024-06-24 07:05:37 +0100

    categories data table and cell actions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 // Toast
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner";
+import ModalProvider from "@/providers/modal-provider";
 
 // Fonts
 const interFont = Inter({ subsets: ["latin"] });
@@ -45,7 +46,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <ModalProvider>{children}</ModalProvider>
             <Toaster />
             <SonnerToaster position="bottom-left" />
           </ThemeProvider>
