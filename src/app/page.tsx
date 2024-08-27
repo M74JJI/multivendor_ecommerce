bb3472ac29fcf3ddc70d8b0e1363db4341682e5f
m74jji <pytaek1@gmail.com>
2024-08-27 21:53:49 +0100

    offer tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import ThemeToggle from "@/components/shared/theme-toggle";
 import { UserButton } from "@clerk/nextjs";
-export default function Home() {
+
+export default function HomePage() {
   return (
-    <div className="p-5">
-      <div className="w-100 flex gap-x-5 justify-end">
-        <UserButton />
-        <ThemeToggle />
-      </div>
-      <h1 className=" text-blue-500 font-barlow">Home page</h1>
+    <div>
+      <UserButton />
     </div>
   );
 }
